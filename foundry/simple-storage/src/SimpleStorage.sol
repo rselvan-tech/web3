afd4f015824b19a3afd95c32bfce43f0c1381ea2
navaras2012 <navaras2012@gmail.com>
2025-08-21 01:00:07 -0400

    18

---
+++ foundry/simple-storage/src/SimpleStorage.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract SimpleStorage{
+    uint public favouriteNumber; 
+
+    function store(uint fnum) public virtual{
+        favouriteNumber=fnum;
+    }
+
+    //view, pure
+    function get() public view returns(uint){
+        return favouriteNumber;
+    }
+
+}
